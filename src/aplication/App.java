0ca15c47838137bfcfe8507e36eecc68622658f0
PauloBrito27 <paulojmbrito27@gmail.com>
2020-04-13 16:33:20 -0300

    Implementação do arquivo db.properties, da DbException personalizada e tambem da classe DB onde são feitas as implementaçoes de conexão com o banco

---
+++ src/aplication/App.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+	}
+
+}
