b30a5bbe2a2d1d6764a82f446c821cc6d36d82e2
PauloBrito27 <paulojmbrito27@gmail.com>
2020-04-14 08:13:54 -0300

    Inserindo vendedor e recebendo o ID criado no banco, Tambem um exemplo de criar dois novos departamentos recebendo seus ID

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -2,9 +2,11 @@ package aplication;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
+import com.mysql.jdbc.Statement;
 
 import db.DB;
 
@@ -17,23 +19,40 @@ public class App {
 
 		try {
 			conn = DB.getConnection();
-			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId )"
-					+ "VALUES (?,?,?,?,?)");
 			
-			st.setString(1, "Paulo José");
-			st.setString(2, "Paulo@email.com");
+			//Inserindo um vendedor e recebendo o numero de ID criado no banco
+			/*
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId )"
+					+ "VALUES (?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, "Davi José");
+			st.setString(2, "Davi@email.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("27/10/1980").getTime()));
 			st.setDouble(4, 4500.00);
 			st.setInt(5, 2);
-
+	       */
+			
+			//teste de criar dois novos departamentos
+			st = conn.prepareStatement(
+					"INSERT into department (Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS
+					);
+						
 			int linhasAlteradas = st.executeUpdate();
-
-			System.out.println("Feito, alteradas " + linhasAlteradas + " linhas");
+			
+			if(linhasAlteradas > 0 ) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Alterado vendedor com id= "+ id);
+				}
+			}else {
+				System.out.println("Não foram alterados nenhuma linha do banco");
+			}
+			
 		}
 		catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		catch (ParseException e) {
 			System.out.println(e.getMessage());
 		}
 		finally {
