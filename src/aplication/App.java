3dbbb1388257d9bc592d5d72a4ea21c06a6ea973
PauloBrito27 <paulojmbrito27@gmail.com>
2020-04-14 07:48:55 -0300

    Inserindo dados com PreparedStatement recebendo o numero de linhas afetadas

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -1,34 +1,44 @@
 package aplication;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import db.DB;
 
 public class App {
 
 	public static void main(String[] args) {
-
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
 
 		try {
 			conn = DB.getConnection();
-			st = conn.createStatement();
-			rs = st.executeQuery("select * from department");
-
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + " " + rs.getString("Name"));
-			}
-		} catch (SQLException e) {
-			System.out.println(e);
-		} finally {
-			DB.closeStatement(st);
-			DB.closeResultSet(rs);
-			DB.closeConnection();
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId )"
+					+ "VALUES (?,?,?,?,?)");
 			
+			st.setString(1, "Paulo José");
+			st.setString(2, "Paulo@email.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("27/10/1980").getTime()));
+			st.setDouble(4, 4500.00);
+			st.setInt(5, 2);
+
+			int linhasAlteradas = st.executeUpdate();
+
+			System.out.println("Feito, alteradas " + linhasAlteradas + " linhas");
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		catch (ParseException e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
 		}
 	}
 
