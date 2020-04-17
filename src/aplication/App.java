c4477bf8ad857f7ecb6e7f8022d86da0c8628fba
PauloBrito27 <paulojmbrito27@gmail.com>
2020-04-17 12:38:46 -0300

    tratamento de integridade com multiplas operações no banco. ou faz tudo, ou volta ao estado anterior

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -1,8 +1,8 @@
 package aplication;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 import db.DbIntegrityException;
@@ -13,21 +13,37 @@ public class App {
 	public static void main(String[] args) {
 
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
 		
 		try {
 			
 			conn = DB.getConnection();
-			st = conn.prepareStatement("DELETE from department WHERE Id = ?");
 			
-			st.setInt(1, 2);
-			int linhasAfetadas = st.executeUpdate();
-			System.out.println("linhas afetadas = "+ linhasAfetadas);
+			conn.setAutoCommit(false); //Impede que sejam realizadas transações até o progtramador habilitar
+			
+			
+			st = conn.createStatement();
+			
+			int a = st.executeUpdate("UPDATE seller SET BaseSalary = 2000 WHERE DepartmentId = 2");
+			int b = st.executeUpdate("UPDATE seller SET BaseSalary = 1000 WHERE DepartmentId = 1");
+			
+			
+			conn.commit();//Habilita a realização das transações, evitando que parte das transações não ocorra.
+			
+			
+			System.out.println("a->" + a);
+			System.out.println("b->" + b);
 			
 		}catch(SQLException e) {
+			
+			//Aqui é feito um tratamento para que o banco volte a operação caso algo já tenha sido alterado
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 			throw new DbIntegrityException(e.getMessage());
 		}finally {
-			
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
